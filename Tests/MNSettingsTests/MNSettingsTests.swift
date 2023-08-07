b03fad187f4a17bbe083d5782020dec6a7358a69
Ido Rabin <ido@sync.me>
2023-08-07 14:22:20 +0300

    Initial Commit

---
+++ Tests/MNSettingsTests/MNSettingsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MNSettings
+
+final class MNSettingsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MNSettings().text, "Hello, World!")
+    }
+}
